2d24e483ec0f1f21e06ed68c958e08b68fa2dc95
CarlsKe <fernancarldiaz@outlook.com>
2018-03-23 16:59:26 -0600

    feat: added settings nginx

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # docker-django
+
+```shell
+docker run -d --name demo -p 80:80 -v "$(pwd)"/wheelhouse:/usr/share/nginx/html temaplte
+```
--- superhero/settings.py
+++ superhero/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7vwrfy1^0or-gx=*+_wt@ggeaye_9#jwep$s20gpgb)6*$d*y%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.17.0.2']
 
 
 # Application definition
