dce9f4ada0b92e48b966668311b21561d7fc6fbe
CarlsKe <fernancarldiaz@outlook.com>
2018-04-17 13:35:04 -0500

    refactor: changed to django 2.0 :beers

--- dockeando/wsgi.py
+++ dockeando/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for superhero project.
+WSGI config for dockeando project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "superhero.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dockeando.settings")
 
 application = get_wsgi_application()
